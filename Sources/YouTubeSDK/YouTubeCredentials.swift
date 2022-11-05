747af94e475ffb52878a11f67934cc6fab0b973b
Muhammad <muhammadbassio@gmail.com>
2022-11-05 23:02:27 +0200

    Update YouTubeCredentials.swift

--- Sources/YouTubeSDK/YouTubeCredentials.swift
+++ Sources/YouTubeSDK/YouTubeCredentials.swift
@@ -10,15 +10,31 @@ import Foundation
 public struct YouTubeAPIKey {
 	public var apiKey: String
 	public var bundleId: String?
+	
+	public init(apiKey: String, bundleId: String? = nil) {
+		self.apiKey = apiKey
+		self.bundleId = bundleId
+	}
 }
 
 public struct YouTubeClientId {
 	public var clientId: String
 	public var clientSecret: String
 	public var redirectURL: String
+	
+	public init(clientId: String, clientSecret: String, redirectURL: String) {
+		self.clientId = clientId
+		self.clientSecret = clientSecret
+		self.redirectURL = redirectURL
+	}
 }
 
 public struct YouTubeCredentials {
 	public var apiKey: YouTubeAPIKey
 	public var clientId: YouTubeClientId?
+	
+	public init(apiKey: YouTubeAPIKey, clientId: YouTubeClientId? = nil) {
+		self.apiKey = apiKey
+		self.clientId = clientId
+	}
 }
